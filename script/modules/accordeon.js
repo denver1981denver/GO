340fe1636f6f47457a3e3ed0f120f0de68284a87
Denis <dennik1981@Gmail.com>
2024-02-17 15:08:07 +0300

    fix1

--- script/modules/accordeon.js
+++ script/modules/accordeon.js
@@ -2,6 +2,14 @@ const items = document.querySelectorAll('.faq__item ');
 const buttons = document.querySelectorAll('.faq__btn');
 const textWrapper = document.querySelectorAll('.faq__text-wrapper');
 
+items.forEach((btn, i) => {
+  if (i % 2 === 0 || i === 0) {
+    btn.dataset.color = 'even';
+  } else {
+    btn.dataset.color = 'odd';
+  }
+})
+
 let heightWrapper = 0;
 
 textWrapper.forEach(elem => {
@@ -18,8 +26,17 @@ buttons.forEach((btn, index) => {
           items[i].classList.contains('faq__item-active') ?
           '' : `${heightWrapper}px`
         items[i].classList.toggle('faq__item-active');
+       
+        if (items[i].getAttribute('data-color') === 'even') {
+           items[i].classList.toggle('faq__close-purple');
+       } 
+       if (items[i].getAttribute('data-color') === 'odd') {
+            items[i].classList.toggle('faq__close-white');
+          }
       } else {
         items[i].classList.remove('faq__item-active');
+        items[i].classList.remove('faq__close-purple');
+        items[i].classList.remove('faq__close-white');
         textWrapper[i].style.height = '';
       }
     }
