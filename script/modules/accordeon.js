60578ef734e32e2137248da37fea278d1ddb22cc
Denis <dennik1981@Gmail.com>
2024-03-05 14:18:37 +0300

    implemented accordion and modal window

--- script/modules/accordeon.js
+++ script/modules/accordeon.js
@@ -1,13 +1,13 @@
-const items = document.querySelectorAll(".faq__item ");
-const buttons = document.querySelectorAll(".faq__btn");
-const textWrapper = document.querySelectorAll(".faq__text-wrapper");
-const container = document.querySelectorAll(".faq__container");
+const items = document.querySelectorAll('.faq__item ');
+const buttons = document.querySelectorAll('.faq__btn');
+const textWrapper = document.querySelectorAll('.faq__text-wrapper');
+const body = document.querySelector('.page');
 
-items.forEach((btn, i) => {
+items.forEach((item, i) => {
   if (i % 2 === 0 || i === 0) {
-    btn.dataset.color = "even";
+    item.dataset.color = 'even';
   } else {
-    btn.dataset.color = "odd";
+    item.dataset.color = 'odd';
   }
 });
 
@@ -20,35 +20,37 @@ textWrapper.forEach((elem) => {
 });
 
 buttons.forEach((btn, index) => {
-  btn.addEventListener("click", (e) => {
+  btn.addEventListener('click', (e) => {
     for (let i = 0; i < items.length; i += 1) {
       if (index === i) {
-        textWrapper[i].style.height = items[i].classList.contains(
-          "faq__item-active"
-        )
-          ? ""
-          : `${heightWrapper}px`;
-        items[i].classList.toggle("faq__item-active");
+        textWrapper[i].style.height = items[i].classList.contains('faq__item-active') ?
+'' : `${heightWrapper}px`;
+        items[i].classList.toggle('faq__item-active');
 
-        if (items[i].getAttribute("data-color") === "even") {
-          items[i].classList.toggle("faq__close-purple");
+        if (items[i].getAttribute('data-color') === 'even') {
+          items[i].classList.toggle('faq__close-purple');
         }
-        if (items[i].getAttribute("data-color") === "odd") {
-          items[i].classList.toggle("faq__close-white");
+
+        if (items[i].getAttribute('data-color') === 'odd') {
+          items[i].classList.toggle('faq__close-white');
         }
-console.log(e.target);
-        // if (e.target !== items) {
-        //   items[i].classList.remove("faq__item-active");
-        //   items[i].classList.remove("faq__close-purple");
-        //   items[i].classList.remove("faq__close-white");
-        //   textWrapper[i].style.height = "";
-        // }
       } else {
-        items[i].classList.remove("faq__item-active");
-        items[i].classList.remove("faq__close-purple");
-        items[i].classList.remove("faq__close-white");
-        textWrapper[i].style.height = "";
+        items[i].classList.remove('faq__item-active');
+        items[i].classList.remove('faq__close-purple');
+        items[i].classList.remove('faq__close-white');
+        textWrapper[i].style.height = '';
       }
+
+      body.addEventListener('click', e => {
+        if (!e.target.closest('.faq__item')) {
+          items[i].classList.remove('faq__item-active');
+          items[i].classList.remove('faq__close-purple');
+          items[i].classList.remove('faq__close-white');
+          textWrapper[i].style.height = '';
+        }
+      });
     }
   });
 });
+
+
--- script/modules/modal.js
+++ script/modules/modal.js
@@ -1,23 +1,13 @@
-const formOverlay = document.querySelector(".overlay-modal");
-const btnAdd = document.querySelector(".call-order-btn");
+const formOverlay = document.querySelector('.overlay-modal');
+const btnAdd = document.querySelector('.call-order-btn');
 
-const modalControl = (btnAdd, formOverlay) => {
-  const openModal = () => {
-    formOverlay.classList.add("is-visible");
-  };
-  const closeModal = () => {
-    formOverlay.classList.remove("is-visible");
-  };
-  btnAdd.addEventListener("click", openModal);
-  formOverlay.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target === formOverlay || target.closest(".modal-form__close")) {
-      closeModal();
-    }
-  });
-  return {
-    closeModal,
-  };
-};
+btnAdd.addEventListener('click', () => {
+  formOverlay.classList.add('is-visible');
+});
 
-modalControl(btnAdd, formOverlay);
+formOverlay.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target === formOverlay || target.closest('.modal-form__close')) {
+    formOverlay.classList.remove('is-visible');
+  }
+});
